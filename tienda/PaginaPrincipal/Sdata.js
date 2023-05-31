13ad27113d4fd1c60fa23fc9761398334620b879
luzmore12 <132724237+luzmore12@users.noreply.github.com>
2023-05-30 20:36:01 -0500

    Add files via upload

---
+++ tienda/PaginaPrincipal/Sdata.js
@@ -0,0 +1,27 @@
+const Sdata = [
+  {
+    id: 1,
+    title: "10% de descuento para su primera compra",
+    desc:  "disfruta un aroma natura y que mejor si compartes una flor que alegre tu dia",,
+    cover: "./images//SlideCard/canastas de lirio.png",
+  },
+  {
+    id: 2,
+    title: "10% de descuento para su primera compra",
+    desc:  "disfruta un aroma natura y que mejor si compartes una flor que alegre tu dia",
+    cover: "./images/SlideCard/flor de primavera.png,
+  },
+  {
+    id: 3,
+    title: "10% de descuento para su primera compra",
+    desc: "disfruta un aroma natura y que mejor si compartes una flor que alegre tu dia",
+    cover: "./images/SlideCard/margarita para enamorar.png",
+  },
+  {
+    id: 4,
+    title: "10% de descuento para su primera compra",
+    desc: "disfruta un aroma natura y que mejor si compartes una flor que alegre tu dia",
+    cover: "./images/SlideCard/petalos floral.png",
+  },
+]
+export default Sdata
